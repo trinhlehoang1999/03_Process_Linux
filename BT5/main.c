4728de3cbc54be8768e5806162d9751776b19350
hoang <trinhlehoang1801@gmail.com>
2025-02-18 20:07:19 +0700

    Update project

--- BT5/main.c
+++ BT5/main.c
@@ -5,7 +5,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-void create_zombie(void) {
+int create_zombie(void) {
     pid_t pid = fork();
     if (pid < 0) {
         printf("fork failed\n");
@@ -13,31 +13,32 @@ void create_zombie(void) {
     if (pid == 0) {
         // Tiến trình con: kết thúc ngay để trở thành zombie.
         printf("Tiến trình con với PID = %d, PPID = %d\n", getpid(), getppid());
-        sleep(3);
+        sleep(2);
         printf("Tiến trình con kết thúc.\n");
     } else {
         // Tiến trình cha: ngủ để giữ tiến trình zombie trong bảng tiến trình.
         printf("tiến trình parent có PID = %d. Tiến trình con có PID = %d).\n", getpid(), pid);
-        sleep(10);
+        sleep(5);
 
     }
 }
 
 
-void create_orphan() {
+int create_orphan(void) {
     pid_t pid = fork();
     if (pid < 0) {
         printf("fork failed\n");
+        return -1;
     }
     if (pid == 0) {
         // Tiến trình con: ngủ lâu để sau khi tiến trình cha kết thúc,
         printf("Tiến trình con PID = %d, PPID ban đầu = %d\n", getpid(), getppid());
-        sleep(10);
+        sleep(5);
         printf("\nTiến trình con sau khi cha kết thúc: PID = %d, PPID mới = %d\n", getpid(), getppid());
     } else {
         // Tiến trình cha: kết thúc trước tiến trình con
         printf("Tiến trình Parent với PID = %d. Tiến trình con PID = %d sẽ trở thành orphan.\n", getpid(), pid);
-        sleep(3);
+        sleep(2);
         printf("Tiến trình cha kết thúc.\n");
     }
 }
@@ -45,6 +46,7 @@ void create_orphan() {
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         printf("chỉ được nhập 1 đối số zombie or orphan\n");
+        return -1;
     }
     
     if (strcmp(argv[1], "zombie") == 0) {
@@ -52,7 +54,7 @@ int main(int argc, char *argv[]) {
     } else if (strcmp(argv[1], "orphan") == 0) {
         create_orphan();
     } else {
-        fprintf(stderr, "Đối số không hợp lệ. Sử dụng 'zombie' hoặc 'orphan'.\n");
+        printf("Đối số không hợp lệ. Sử dụng 'zombie' hoặc 'orphan'\n");
     }
     
     return 0;
